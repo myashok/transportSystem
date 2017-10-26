6c6f36420a96be2ab227f694061b6702b96e91f5
phoenikx <nik211012@gmail.com>
2017-10-26 10:30:23 +0530

    Add views for access denied, view requests

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns=[
@@ -6,5 +8,9 @@ urlpatterns=[
     url(r'^login$',views.login_view,name='login'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^transport_request',views.transport_request,name='transport_request'),
-    url(r'^transport_request/(?P<id>\d+)',views.view_request,name='view_request')
+    url(r'^transport_request/(?P<id>\d+)',views.view_request,name='view_request'),
+    url(r'^my_requests$',views.my_requests,name='my_requests'),
+    url(r'^staff$',views.staff_home,name='staff_home'),
+    url(r'^staff/pending_requests$',views.pending_requests,name='pending_requests'),
+    url(r'^access_denied$',TemplateView.as_view(template_name='access_denied.html'),name='access_denied')
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -1,10 +1,14 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import Group
 from django.forms import ModelForm, DateInput
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
 from main_site.models import Request
+import logging
+logger = logging.getLogger(__name__)
+def has_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    return True if group in user.groups.all() else False
 
 class RequestForm(ModelForm):
     class Meta:
@@ -16,8 +20,11 @@ class RequestForm(ModelForm):
             'time_of_journey': DateInput(attrs={'type':'time'}),
         }
 
+def access_denied(request):
+    return render(request,'access_denied.html')
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'user_home.html')
 
 def login_view(request):
     if request.method=='POST':
@@ -33,7 +40,15 @@ def login_view(request):
         else:
             return render(request,'registration/login.html',{'error':'Invalid login credentials'})
     else:
-        return render(request,'registration/login.html')
+        if request.user.is_authenticated:
+            logger.info(request.user.groups.all())
+            if has_group(request.user,'Admin'):
+                return redirect('staff_home')
+            else:
+                return redirect('home')
+        else:
+            return render(request,'registration/login.html')
+
 def logout_view(request):
     logout(request)
     return redirect('home')
@@ -43,16 +58,36 @@ def transport_request(request):
     if request.method=='POST':
         form=RequestForm(request.POST)
         if form.is_valid():
-            #add to database and redirectg
-            return redirect('view_request',id=1)
+            req=Request(form)
+            req.save()
         else:
             return render(request,'request_vehicle.html',{'form':form})
     else:
         form=RequestForm()
         return render(request,'request_vehicle.html',{'form':form})
 
+def my_requests(request):
+    requests=Request.objects.filter(user=request.user)
+    return render(request,'my_requests.html',{'requests':requests})
+
 def view_request(request,id):
+
     temp=Request.objects.get({'id':id})
     if temp.user is not request.user:
         return
     return render()
+
+#####staff views#####
+def not_in_staff_group(user):
+    if user:
+        return user.groups.filter(name='Staff').count() ==0
+    return True
+#@login_required(login_url='login')
+@permission_required('request.user.is_staff',login_url='access_denied')
+def staff_home(request):
+    return render(request,'staff/staff_home.html')
+
+def pending_requests(request):
+    pending=Request.objects.filter(request_status='pending').order_by('date_of_journey')
+    return render('pending.html',{'pending':pending})
+
