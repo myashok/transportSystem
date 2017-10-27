97130f4d1d31596aeaea852196f80be732f85e08
phoenikx <nik211012@gmail.com>
2017-10-27 12:57:07 +0530

    Add view for view and update

--- main_site/urls.py
+++ main_site/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
-
+from main_site.views import edit_request
 from . import views
 
 urlpatterns=[
     url(r'^$',views.home,name='home'),
-    url(r'^login$',views.login_view,name='login'),
-    url(r'^logout/$', views.logout_view, name='logout'),
-    url(r'^transport_request',views.transport_request,name='transport_request'),
-    url(r'^transport_request/(?P<id>\d+)',views.view_request,name='view_request'),
-    url(r'^my_requests$',views.my_requests,name='my_requests'),
+    url(r'^login',views.login_view,name='login'),
+    url(r'^logout', views.logout_view, name='logout'),
+    url(r'^requests/new', views.new_request, name='new_request'),
+    url(r'^requests/(?P<pk>\d+)$', views.view_request, name='view_request'),
+    url(r'^requests$',views.my_requests,name='my_requests'),
+    url(r'^requests/(?P<pk>\d+)/edit$',edit_request.as_view(),name='edit_request'),
     url(r'^staff$',views.staff_home,name='staff_home'),
     url(r'^staff/pending_requests$',views.pending_requests,name='pending_requests'),
-    url(r'^access_denied$',TemplateView.as_view(template_name='access_denied.html'),name='access_denied')
+    url(r'^accesss_denied$',TemplateView.as_view(template_name='access_denied.html'),name='access_denied')
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 from django.forms import ModelForm, DateInput
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.views.generic import UpdateView
+
 from main_site.models import Request
+from django.http import Http404
 import logging
 logger = logging.getLogger(__name__)
 def has_group(user, group_name):
@@ -53,7 +56,7 @@ def logout_view(request):
     return redirect('home')
 
 @login_required(login_url='login')
-def transport_request(request):
+def new_request(request):
     if request.method=='POST':
         form=RequestForm(request.POST)
         if form.is_valid():
@@ -62,22 +65,40 @@ def transport_request(request):
             instance.save()
             return redirect('my_requests')
         else:
-            return render(request,'request_vehicle.html',{'form':form})
+            return render(request, 'new_request.html', {'form':form})
     else:
         form=RequestForm()
-        return render(request,'request_vehicle.html',{'form':form})
+        return render(request, 'new_request.html', {'form':form})
+
 
 def my_requests(request):
     requests=Request.objects.filter(user=request.user)
     return render(request,'my_requests.html',{'requests':requests})
 
-def view_request(request,id):
 
-    temp=Request.objects.get({'id':id})
-    if temp.user is not request.user:
-        return
-    return render()
+def view_request(request,pk):
+    try:
+        req = Request.objects.get(pk=pk)
+        if req.user!=request.user:
+            print(request.user.groups.all())
+            if request.user.groups.filter(name='Transport_Admin').exists():
+                return render(request, 'view_request.html', {'req': req})
+            else:
+                return redirect('/access_denied')
 
+        else:
+            return render(request, 'view_request.html', {'req': req})
+
+    except Request.DoesNotExist:
+        raise Http404
+
+
+
+
+def delete_request(request,ID):
+    req=Request.objects.get({'id':ID})
+    req.delete()
+    return redirect('user_home')
 #####staff views#####
 def not_in_staff_group(user):
     if user:
@@ -91,4 +112,11 @@ def staff_home(request):
 def pending_requests(request):
     pending=Request.objects.filter(request_status='pending').order_by('date_of_journey')
     return render('pending.html',{'pending':pending})
+
+class edit_request(UpdateView): #Note that we are using UpdateView and not FormView
+    model = Request
+    fields = ['date_of_journey', 'time_of_journey', 'request_type', 'description',
+              'source', 'destination', 'is_return_journey']
+    template_name = "new_request.html"
+    success_url = '/requests'
 
