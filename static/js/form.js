2dcb6332e9720fa4c1765fd3183803afcd065dd2
myashok <iit2014121@iiita.ac.in>
2017-11-07 00:58:11 +0530

    made clock 24 hour

--- static/js/form.js
+++ static/js/form.js
@@ -1,6 +1,14 @@
 $("input").addClass("form-control");
 $("select").addClass("form-control");
 $("textarea").addClass("form-control");
-$('#id_time_of_journey').timepicki();
+$('#id_time_of_journey').timepicki({
+		show_meridian:false,
+		min_hour_value:0,
+		max_hour_value:23,
+		step_size_minutes:15,
+		overflow_minutes:true,
+		increase_direction:'up',
+		disable_keyboard_mobile: true
+});
 $('#id_date_of_journey').dateTimePicker();
 $("#id_is_return_journey").css({"float": "left", "clear": "both", "margin-top": "-25px",});
