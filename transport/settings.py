befab4121dac2a3af678c6b8af06907307a25251
phoenikx <nik211012@gmail.com>
2017-11-07 14:56:53 +0530

    Final wrap up for deployment

--- transport/settings.py
+++ transport/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_URL = '/static/'
+STATIC_URL = '/'
 
 
 MEDIA_URL = '/media/'
