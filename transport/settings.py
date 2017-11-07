de882a138b5da5fa28a402a402ed65354ececfff
phoenikx <nik211012@gmail.com>
2017-11-07 14:55:11 +0530

    Final wrap up for deployment

--- transport/settings.py
+++ transport/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
@@ -130,3 +130,10 @@ EMAIL_HOST_USER = 'iit2014129@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'iit2014129@gmail.com'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
