49da48dfa83a96f4a6ac94d69997af831fca7e47
phoenikx <nik211012@gmail.com>
2017-11-07 14:43:45 +0530

    Add static root

--- transport/settings.py
+++ transport/settings.py
@@ -117,11 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
-STATIC_URL = '/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
