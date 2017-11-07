0a9475a966339f54901bf8f70e45a442d06c87a0
phoenikx <nik211012@gmail.com>
2017-11-07 14:45:30 +0530

    Change settings.py

--- transport/settings.py
+++ transport/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_URL = '/'
 
 
 MEDIA_URL = '/media/'
