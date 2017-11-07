3b475afe88dfdf42683880f57fc3a63fd234c07b
phoenikx <nik211012@gmail.com>
2017-11-07 16:02:56 +0530

    Fix after a try to deploy

--- transport/settings.py
+++ transport/settings.py
@@ -117,11 +117,9 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
